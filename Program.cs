549cada7603e57e30711703c5a2e4b084ef70fb5
Tariq Alsharief <tariqalsharief@live.com>
2024-11-15 17:03:46 +0300

    Adding New Function
    Say My Name Function

--- Program.cs
+++ Program.cs
@@ -8,11 +8,18 @@ namespace GithubTest
 {
     internal class Program
     {
+
+        private static string SayMyName(string name)
+        {
+            return name;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Github!! T");
+            Console.WriteLine("Hello " + SayMyName("Tariq"));
 
             Console.ReadKey();
         }
     }
 }
+
